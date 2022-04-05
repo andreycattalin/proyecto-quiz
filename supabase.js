c6f4fe66d234e0a1a119dcc7be1380d3e8408e47
Andrey <cattalin.htc@gmail.com>
2022-04-05 12:36:50 +0200

    --wip-- [skip ci]

--- supabase.js
+++ supabase.js
@@ -1,9 +1,34 @@
 const { createClient } = supabase;
 const mySupabase = createClient('https://nrxjmfurruizoaevsnod.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yeGptZnVycnVpem9hZXZzbm9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkxNDk1ODQsImV4cCI6MTk2NDcyNTU4NH0.KrAQ1wlr9An24Xzu_fnVFnjHB0UjuaF52JFBdhLrBIA')
 
+let currentQuestion = 0;
+let allQuestions;
+
 async function loadDatabase() {
     const { data, error } = await mySupabase.from('Questions').select()
-    console.log(data)
+    console.log("Data",data)
+
+    allQuestions = data
+
+    document.getElementById("total").innerHTML = "Pregunta " + (currentQuestion + 1) + " de " + data.length
+
+    setCurrentQuestion()
+}
+
+function setCurrentQuestion() {
+    
+    document.getElementById("title").innerHTML = allQuestions[currentQuestion].question
+
+    var result = ""
+    allQuestions[currentQuestion].answers.forEach(element => {
+        let myAnswer = '<button class="btn btn-outline-primary" type="button">'+element+'</button>'
+        result = result + myAnswer
+
+        console.log("DALE", result)
+    });
+
+    document.getElementById("answers").innerHTML = result
+
 }
 
 loadDatabase();
